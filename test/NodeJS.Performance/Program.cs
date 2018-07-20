29de6cd1a19bf7646211b54bffac0e655afde8cd
JeremyTCD <jeremytanchongdao@gmail.com>
2018-07-20 22:16:12 +0800

    Added performance tests

---
+++ test/NodeJS.Performance/Program.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Jering.JavascriptUtils.NodeJS.Performance
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<InvokeFromFile>();
+        }
+
+        public class DummyResult
+        {
+            public string Result { get; set; }
+        }
+    }
+}
---
+++ test/NodeJS.Performance/dummyModule.js
@@ -0,0 +1,2 @@
+﻿// Used by HttpNodeJSServiceBenchmarks
+module.exports = (callback, resultString) => callback(null, { result: resultString });
