e962746ff089aac8c6fd67663eb1ca067a55ff25
JeremyTCD <jeremytanchongdao@gmail.com>
2018-07-20 10:37:49 +0800

    Added lstat monkey patch

--- src/NodeJS/Javascript/Servers/OutOfProcess/Http/HttpServer.ts
+++ src/NodeJS/Javascript/Servers/OutOfProcess/Http/HttpServer.ts
@@ -17,6 +17,9 @@ demarcateMessageEndings(process.stderr);
 // Start auto-termination loop
 exitWhenParentExits(parseInt(args.parentPid), true, 1000);
 
+// Patch lstat - issue explained in this comment: https://github.com/aspnet/JavaScriptServices/issues/1101#issue-241971678
+patchLStat();
+
 // Start server
 const server = http.createServer((req, res) => {
     let bodyChunks = [];
@@ -201,6 +204,55 @@ function processExists(pid: number) {
             throw new Error(`Attempted to check whether process ${pid} was running, but got a permissions error.`);
         }
         return false;
+    }
+}
+
+// https://github.com/aspnet/JavaScriptServices/blob/4763ad5b8c0575f030a3cac8518767f4bd192c9b/src/Microsoft.AspNetCore.NodeServices/TypeScript/Util/PatchModuleResolutionLStat.ts
+function patchLStat() {
+    const appRootDir = process.cwd();
+
+    function patchedLStat(pathToStatLong: string, fsReqWrap?: any) {
+        try {
+            // If the lstat completes without errors, we don't modify its behavior at all
+            return origLStat.apply(this, arguments);
+        } catch (ex) {
+            const shouldOverrideError =
+                ex.message.startsWith('EPERM')                     // It's a permissions error
+                && typeof appRootDirLong === 'string'
+                && appRootDirLong.startsWith(pathToStatLong)       // ... for an ancestor directory
+                && ex.stack.indexOf('Object.realpathSync ') >= 0;   // ... during symlink resolution
+            if (shouldOverrideError) {
+                // Fake the result to give the same result as an 'lstat' on the app root dir.
+                // This stops Node failing to load modules just because it doesn't know whether
+                // ancestor directories are symlinks or not. If there's a genuine file
+                // permissions issue, it will still surface later when Node actually
+                // tries to read the file.
+                return origLStat.call(this, appRootDir, fsReqWrap);
+            } else {
+                // In any other case, preserve the original error
+                throw ex;
+            }
+        }
+    };
+
+    // It's only necessary to apply this workaround on Windows
+    let appRootDirLong: string = null;
+    let origLStat: Function = null;
+    if (/^win/.test(process.platform)) {
+        try {
+            // Get the app's root dir in Node's internal "long" format (e.g., \\?\C:\dir\subdir)
+            appRootDirLong = (path as any)._makeLong(appRootDir);
+
+            // Actually apply the patch, being as defensive as possible
+            const bindingFs = (process as any).binding('fs');
+            origLStat = bindingFs.lstat;
+            if (typeof origLStat === 'function') {
+                bindingFs.lstat = patchedLStat;
+            }
+        } catch (ex) {
+            // If some future version of Node throws (e.g., to prevent use of process.binding()),
+            // don't apply the patch, but still let the application run.
+        }
     }
 }
 
