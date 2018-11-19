393a02298f0f8139bcc95d59957559c0e4a3919f
JeremyTCD <jeremytanchongdao@gmail.com>
2018-11-19 12:03:57 +0800

    Added NodeJSProcess

---
+++ src/NodeJS/NodeJSServiceImplementations/OutOfProcess/INodeJSProcess.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Jering.Javascript.NodeJS
+{
+    /// <summary>
+    /// <para>A thread-safe abstraction for a NodeJS process.</para>
+    /// <para>This abstraction exists for these reasons:</para>
+    /// <list type="bullet">
+    /// <item>Accessing properties of disposed process objects causes exceptions. For example, after calling MyProcess.Dispose(), 
+    /// calling MyProcess.HasExited throws an InvalidOperationException. This abstraction prevents such exceptions by tracking
+    /// the disposed status of the process in a thread safe manner.</item>
+    /// <item>NodeJS processes have an extra state: connected. This abstraction wraps the state together with the process.</item>
+    /// <item>This abstraction allows for more simpler and more thorough tests through mocking.</item>
+    /// </list>
+    /// </summary>
+    public interface INodeJSProcess : IDisposable
+    {
+        /// <summary>
+        /// Gets a boolean value indicating whether or not this NodeJS process is connected.
+        /// </summary>
+        bool Connected { get; }
+
+        /// <summary>
+        /// Gets a boolean value indicating whether or not this NodeJS process has been exited.
+        /// </summary>
+        bool HasExited { get; }
+
+        /// <summary>
+        /// <para>Gets a string describing this NodeJS process's exit code.</para>
+        /// <para>If the process has not exited, returns "Process has not exited".</para>
+        /// <para>If the process has been disposed, returns "Process has been disposed".</para>
+        /// </summary>
+        string ExitCode { get; }
+
+        /// <summary>
+        /// Disposes of this NodeJS process if it is not connected.
+        /// </summary>
+        void DisposeIfNotConnected();
+
+        /// <summary>
+        /// Adds a <see cref="Process.OutputDataReceived"/> handler to this NodeJS process.
+        /// </summary>
+        /// <param name="dataReceivedEventHandler">The handler to add.</param>
+        void AddOutputDataReceivedHandler(DataReceivedEventHandler dataReceivedEventHandler);
+
+        /// <summary>
+        /// Adds a <see cref="Process.ErrorDataReceived"/> handler to this NodeJS process.
+        /// </summary>
+        /// <param name="dataReceivedEventHandler">The handler to add.</param>
+        void AddErrorDataReceivedHandler(DataReceivedEventHandler dataReceivedEventHandler);
+
+        /// <summary>
+        /// Sets this NodeJS process's connected state to true.
+        /// </summary>
+        void SetConnected();
+
+        /// <summary>
+        /// Begins asynchronous read operations on the redirected <see cref="Process.StandardError"/> stream of the application.
+        /// </summary>
+        void BeginErrorReadLine();
+
+        /// <summary>
+        /// Begins asynchronous read operations on the redirected <see cref="Process.StandardOutput"/> stream of the application.
+        /// </summary>
+        void BeginOutputReadLine();
+    }
+}
---
+++ src/NodeJS/NodeJSServiceImplementations/OutOfProcess/NodeJSProcess.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Jering.Javascript.NodeJS
+{
+    /// <summary>
+    /// The default implementation of <see cref="INodeJSProcess"/>.
+    /// </summary>
+    public class NodeJSProcess : INodeJSProcess
+    {
+        private readonly Process _process;
+        private readonly object _lock = new object();
+        private bool _connected;
+        private bool _disposed;
+
+        /// <summary>
+        /// Creates a <see cref="NodeJSProcess"/> instance.
+        /// </summary>
+        /// <param name="process">The NodeJS process.</param>
+        public NodeJSProcess(Process process)
+        {
+            _process = process ?? throw new ArgumentNullException(nameof(process));
+        }
+
+        /// <inheritdoc />
+        public void AddOutputDataReceivedHandler(DataReceivedEventHandler dataReceivedEventHandler)
+        {
+            _process.OutputDataReceived += dataReceivedEventHandler;
+        }
+
+        /// <inheritdoc />
+        public void AddErrorDataReceivedHandler(DataReceivedEventHandler dataReceivedEventHandler)
+        {
+            _process.ErrorDataReceived += dataReceivedEventHandler;
+        }
+
+        /// <inheritdoc />
+        public void BeginOutputReadLine()
+        {
+            _process.BeginOutputReadLine();
+        }
+
+        /// <inheritdoc />
+        public void BeginErrorReadLine()
+        {
+            _process.BeginErrorReadLine();
+        }
+
+        /// <inheritdoc />
+        public void SetConnected()
+        {
+            lock (_lock)
+            {
+                _connected = true;
+            }
+        }
+
+        /// <inheritdoc />
+        public string ExitCode
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (HasExited)
+                    {
+                        return _disposed ? "Process has been disposed" : _process.ExitCode.ToString();
+                    }
+
+                    return "Process has not exited";
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public bool HasExited
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _disposed || _process.HasExited;
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public bool Connected
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return !HasExited && _connected;
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public void DisposeIfNotConnected()
+        {
+            lock (_lock)
+            {
+                if (!Connected)
+                {
+                    Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Kills and disposes of this instance's underlying NodeJS <see cref="Process"/>.
+        /// </summary>
+        public void Dispose()
+        {
+            lock (_lock)
+            {
+                _process.Kill();
+                // Give async output some time to push its messages
+                _process.WaitForExit(500);
+                // After process has exited, dispose of instance to release handle - https://docs.microsoft.com/en-sg/dotnet/api/system.diagnostics.process?view=netframework-4.7.1
+                _process.Dispose();
+
+                _disposed = true;
+                _connected = false;
+            }
+        }
+    }
+}
