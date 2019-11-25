56c7a699fabe01e95db22a062e50e1ead7b16bd1
DaniilSokolyuk <danoov@gmail.com>
2019-11-25 13:15:18 +0300

    Rolled back changes in benchmark

--- perf/NodeJS/Benchmarks.cs
+++ perf/NodeJS/Benchmarks.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 namespace Jering.Javascript.NodeJS.Performance
 {
     [MemoryDiagnoser]
-    [InProcess()]
     public class Benchmarks
     {
         private ServiceProvider _serviceProvider;
