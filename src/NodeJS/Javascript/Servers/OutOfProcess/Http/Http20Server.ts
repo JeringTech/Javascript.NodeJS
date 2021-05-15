3fe875e187c180461fda831db600f6ed4101fb2f
JeremyTCD <jeremytanchongdao@gmail.com>
2021-05-15 12:22:18 +0800

    Updated javascript
    - Bumped dependency versions.
    - Moved de-duplicated logic shared by HTTP servers.

--- src/NodeJS/Javascript/Servers/OutOfProcess/Http/Http20Server.ts
+++ src/NodeJS/Javascript/Servers/OutOfProcess/Http/Http20Server.ts
@@ -1,46 +1,43 @@
 // The typings for module are incomplete and can't be augmented, so import as any.
 const Module = require('module');
-import * as path from 'path';
 import * as http2 from 'http2';
-import * as stream from 'stream';
 import { AddressInfo } from 'net';
+import * as path from 'path';
+import * as stream from 'stream';
 import InvocationRequest from '../../../InvocationData/InvocationRequest';
 import ModuleSourceType from '../../../InvocationData/ModuleSourceType';
+import { getTempIdentifier, respondWithError, setup } from './Shared';
 
-// Parse arguments
-const args: { [key: string]: string } = parseArgs(process.argv);
+// Setup
+const [args, projectDir, moduleResolutionPaths] = setup();
 
-// Overwrite writing to output streams
-demarcateMessageEndings(process.stdout);
-demarcateMessageEndings(process.stderr);
+// Start
+startServer();
 
-// Start auto-termination loop
-exitWhenParentExits(parseInt(args.parentPid), true, 1000);
+function startServer(): void {
+    // Create server
+    const server: http2.Http2Server = http2.createServer(serverOnRequestListener);
 
-// Patch lstat - issue explained in this comment: https://github.com/aspnet/JavaScriptServices/issues/1101#issue-241971678
-patchLStat();
+    // By default, on older versions of Node.js, request handling times out after 120 seconds.
+    // This timeout is disabled by default on Node.js v13+. 
+    // Becuase of the older versions, we explicitly disable it.
+    server.setTimeout(0);
 
-// Set by NodeJSProcessFactory
-const projectDir = process.cwd();
-const moduleResolutionPaths = generateModuleResolutionPaths(projectDir);
+    // Log timed out connections for debugging
+    server.on('timeout', serverOnTimeout);
 
-// Create server
-const server: http2.Http2Server = http2.createServer(serverOnRequestListener);
+    // Send client error details to client for debugging
+    server.on('sessionError', serverOnSessionError); // Provides a bit more specificity than the error event below
+    server.on('error', serverOnError);
 
-// By default, on older versions of Node.js, request handling times out after 120 seconds.
-// This timeout is disabled by default on Node.js v13+. 
-// Becuase of the older versions, we explicitly disable it.
-server.setTimeout(0);
-
-// Log timed out connections for debugging
-server.on('timeout', serverOnTimeout);
-
-// Send client error details to client for debugging
-server.on('sessionError', serverOnSessionError); // Provides a bit more specificity than the error event below
-server.on('error', serverOnError);
-
-// Start server
-server.listen(parseInt(args.port), 'localhost', serverOnListeningListener);
+    // Start server
+    server.listen(parseInt(args.port), 'localhost', () => {
+        // Signal to HttpNodeHost which loopback IP address (IPv4 or IPv6) and port it should make its HTTP connections on
+        // and that we are ready to process invocations.
+        let info = server.address() as AddressInfo;
+        console.log(`[Jering.Javascript.NodeJS: Listening on IP - ${info.address} Port - ${info.port}]`);
+    });
+}
 
 function serverOnRequestListener(req: http2.Http2ServerRequest, res: http2.Http2ServerResponse) {
     const bodyChunks = [];
@@ -75,8 +72,8 @@ function serverOnRequestListener(req: http2.Http2ServerRequest, res: http2.Http2
                 } else if (invocationRequest.moduleSourceType === ModuleSourceType.Stream ||
                     invocationRequest.moduleSourceType === ModuleSourceType.String) {
                     // Check if already cached
-                    if (invocationRequest.newCacheIdentifier != null) {
-                        const cachedModule = Module._cache[invocationRequest.newCacheIdentifier];
+                    if (invocationRequest.cacheIdentifier != null) {
+                        const cachedModule = Module._cache[invocationRequest.cacheIdentifier];
                         if (cachedModule != null) {
                             exports = cachedModule.exports;
                         }
@@ -94,13 +91,13 @@ function serverOnRequestListener(req: http2.Http2ServerRequest, res: http2.Http2
                         // which Module.load calls internally.
                         newModule._compile(invocationRequest.moduleSource, 'anonymous');
 
-                        if (invocationRequest.newCacheIdentifier != null) {
+                        if (invocationRequest.cacheIdentifier != null) {
                             // Notes on module caching:
                             // When a module is required using require, it is cached in Module._cache using its absolute file path as its key.
                             // When Module._load tries to load the same module again, it first resolves the absolute file path of the module, then it 
                             // checks if the module exists in the cache. Custom keys for in memory modules cause an error at the file resolution step.
                             // To make modules with custom keys requirable by other modules, require must be monkey patched.
-                            Module._cache[invocationRequest.newCacheIdentifier] = newModule;
+                            Module._cache[invocationRequest.cacheIdentifier] = newModule;
                         }
 
                         exports = newModule.exports;
@@ -179,19 +176,6 @@ function serverOnRequestListener(req: http2.Http2ServerRequest, res: http2.Http2
         });
 }
 
-function generateModuleResolutionPaths(projectDirectory: string): string[] {
-    const result: string[] = [path.join(projectDirectory, 'node_modules')];
-    let directory: string = projectDirectory;
-    let lastDirectory: string;
-
-    while ((directory = path.dirname(directory)) !== lastDirectory) { // Once we reach root directory, path.dirname(root directory) returns root directory. E.g. path.dirname('C:/') returns 'C:/'
-        result.push(path.join(directory, 'node_modules'));
-        lastDirectory = directory;
-    }
-
-    return result;
-}
-
 // Send error details to client for debugging
 function serverOnSessionError(error: Error) {
     console.log(`[Node.js HTTP server] Session error: ${error.stack}`);
@@ -205,141 +189,4 @@ function serverOnError(error: Error) {
 // The socket won't actually get closed (the timeout function needs to do that manually).
 function serverOnTimeout() {
     console.log(`[Node.js HTTP server] Ignoring unexpected socket timeout`);
-}
-
-function serverOnListeningListener() {
-    // Signal to HttpNodeHost which loopback IP address (IPv4 or IPv6) and port it should make its HTTP connections on
-    // and that we are ready to process invocations.
-    let info = server.address() as AddressInfo;
-    console.log(`[Jering.Javascript.NodeJS: Listening on IP - ${info.address} Port - ${info.port}]`);
-}
-
-function getTempIdentifier(invocationRequest: InvocationRequest): string {
-    if (invocationRequest.newCacheIdentifier == null) {
-        return `"${invocationRequest.moduleSource.substring(0, 25)}..."`;
-    } else {
-        return invocationRequest.newCacheIdentifier;
-    }
-}
-
-function respondWithError(res: http2.Http2ServerResponse, error: Error | string) {
-    const errorIsString: boolean = typeof error === 'string';
-
-    res.statusCode = 500;
-    res.end(JSON.stringify({
-        errorMessage: errorIsString ? error : (error as Error).message,
-        errorStack: errorIsString ? null : (error as Error).stack
-    }));
-}
-
-// https://github.com/aspnet/JavaScriptServices/blob/0dc570a0c8725e3031ce5a884d7df3cfb75545ba/src/Microsoft.AspNetCore.NodeServices/TypeScript/Util/ArgsUtil.ts
-function parseArgs(args: string[]) {
-    let currentKey = null;
-    const result: any = {};
-    args.forEach(arg => {
-        if (arg.indexOf('--') === 0) {
-            const argName = arg.substring(2);
-            result[argName] = undefined;
-            currentKey = argName;
-        } else if (currentKey !== null) {
-            result[currentKey] = arg;
-            currentKey = null;
-        }
-    })
-
-    return result;
-}
-
-// https://github.com/aspnet/JavaScriptServices/blob/0dc570a0c8725e3031ce5a884d7df3cfb75545ba/src/Microsoft.AspNetCore.NodeServices/TypeScript/Util/ExitWhenParentExits.ts
-function exitWhenParentExits(parentPid: number, ignoreSigint: boolean, pollIntervalMS: number) {
-    setInterval(() => {
-        if (!processExists(parentPid)) {
-            console.log(`[Node.js HTTP server] Parent process (pid: ${parentPid}) exited. Exiting this process...`);
-            process.exit();
-        }
-    }, pollIntervalMS);
-
-    if (ignoreSigint) {
-        process.on('SIGINT', () => {
-            console.log('[Node.js HTTP server] Received SIGINT. Waiting for .NET process to exit...');
-        });
-    }
-}
-
-// https://github.com/aspnet/JavaScriptServices/blob/0dc570a0c8725e3031ce5a884d7df3cfb75545ba/src/Microsoft.AspNetCore.NodeServices/TypeScript/Util/ExitWhenParentExits.ts
-function processExists(pid: number) {
-    try {
-        process.kill(pid, 0);
-        return true;
-    } catch (ex) {
-        if (ex.code === 'EPERM') {
-            throw new Error(`Attempted to check whether process ${pid} was running, but got a permissions error.`);
-        }
-        return false;
-    }
-}
-
-// https://github.com/aspnet/JavaScriptServices/blob/4763ad5b8c0575f030a3cac8518767f4bd192c9b/src/Microsoft.AspNetCore.NodeServices/TypeScript/Util/PatchModuleResolutionLStat.ts
-function patchLStat() {
-    function patchedLStat(pathToStatLong: string, fsReqWrap?: any) {
-        try {
-            // If the lstat completes without errors, we don't modify its behavior at all
-            return origLStat.apply(this, arguments);
-        } catch (ex) {
-            const shouldOverrideError =
-                ex.message.startsWith('EPERM')                     // It's a permissions error
-                && typeof appRootDirLong === 'string'
-                && appRootDirLong.startsWith(pathToStatLong)       // ... for an ancestor directory
-                && ex.stack.indexOf('Object.realpathSync ') >= 0;   // ... during symlink resolution
-            if (shouldOverrideError) {
-                // Fake the result to give the same result as an 'lstat' on the app root dir.
-                // This stops Node failing to load modules just because it doesn't know whether
-                // ancestor directories are symlinks or not. If there's a genuine file
-                // permissions issue, it will still surface later when Node actually
-                // tries to read the file.
-                return origLStat.call(this, projectDir, fsReqWrap);
-            } else {
-                // In any other case, preserve the original error
-                throw ex;
-            }
-        }
-    }
-
-    // It's only necessary to apply this workaround on Windows
-    let appRootDirLong: string = null;
-    let origLStat: Function = null;
-    if (/^win/.test(process.platform)) {
-        try {
-            // Get the app's root dir in Node's internal "long" format (e.g., \\?\C:\dir\subdir)
-            appRootDirLong = (path as any)._makeLong(projectDir);
-
-            // Actually apply the patch, being as defensive as possible
-            const bindingFs = (process as any).binding('fs');
-            origLStat = bindingFs.lstat;
-            if (typeof origLStat === 'function') {
-                bindingFs.lstat = patchedLStat;
-            }
-        } catch (ex) {
-            // If some future version of Node throws (e.g., to prevent use of process.binding()),
-            // don't apply the patch, but still let the application run.
-        }
-    }
-}
-
-function demarcateMessageEndings(outputStream: NodeJS.WritableStream) {
-    const origWriteFunction = outputStream.write;
-    outputStream.write = <any>function (value: any) {
-        // Only interfere with the write if it's definitely a string
-        if (typeof value === 'string') {
-            // Node appends a new line character at the end of the message. This facilitates reading of the stream: the process reading it reads messages line by line -
-            // characters stay in its buffer until a new line character is received. This means that the null terminating character must appear before the last
-            // new line character of the message. This approach is inefficent since it allocates 2 strings.
-            //
-            // TODO consider sending '\0\n' as a demarcator after sending value (profile). Also need to check if logging from worker threads might cause
-            // concurrency issues.
-            arguments[0] = value.slice(0, value.length - 1) + '\0\n';
-        }
-
-        origWriteFunction.apply(this, arguments);
-    };
 }
