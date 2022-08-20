5cc32dac1e6ab4444e8ebf2d2320104adfef2a12
JeremyTCD <jeremytanchongdao@gmail.com>
2022-08-20 14:58:23 +0800

    Added DisposeTrackingSemaphoreSlim

---
+++ test/NodeJS/DisposeTrackingSemaphoreSlimIntegrationTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace Jering.Javascript.NodeJS.Tests
+{
+    public class DisposeTrackingSemaphoreSlimIntegrationTests
+    {
+        [Fact]
+        public void ReleaseIfNotDisposed_DoesNotThrowObjectDisposedExceptionIfCalledAfterObjectIsDisposed()
+        {
+            // Arrange
+            DisposeTrackingSemaphoreSlim testSubject = new(1, 1);
+            testSubject.Dispose();
+
+            // Act and assert
+            testSubject.ReleaseIfNotDisposed(); // Successfuly if this does not throw
+        }
+
+        [Fact]
+        public void ReleaseIfNotDisposed_ReleasesIfObjectHasNotBeenDisposed()
+        {
+            // Arrange
+            DisposeTrackingSemaphoreSlim testSubject = new(0, 1);
+
+            // Act
+            testSubject.ReleaseIfNotDisposed();
+
+            // Assert
+            Assert.Equal(1, testSubject.CurrentCount);
+        }
+    }
+}
